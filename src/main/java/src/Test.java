af90e0172a1c341bdf7e93a9ddfdb4f5e4db923d
Lai-zw <zwlai89@gmail.com>
2021-11-19 10:45:56 +0800

    2021-11-19

---
+++ src/main/java/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = new HashMap();
+        map.put("A", "a");
+        System.out.println(map);
+
+    }
+
+}
