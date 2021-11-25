509ddbe31df7113a902aa350afe1c8f99636a741
Laizw <zwlai89@gmail.com>
2021-11-25 19:46:52 +0800

    2021-11-25

--- src/main/java/src/Test.java
+++ src/main/java/src/Test.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class Test {
 
@@ -8,6 +9,16 @@ public class Test {
         map.put("A", "a");
         System.out.println(map);
 
+        // 接收一个泛型对象或可变成泛型集合，构造一个 Stream 对象。
+        Stream<String> stringStream = Stream.of("a", "b", "c");
+        // 连接两个 Stream ，不改变其中任何一个 Steam 对象，返回一个新的 Stream 对象
+        Stream<String> a = Stream.of("a","b","c");
+        Stream<String> b = Stream.of("d","e");
+        Stream<String> c = Stream.concat(a,b);
+
+        Stream<Integer> integerStream = Stream.of(1, 2, 10, 12);
+        Integer integer = integerStream.max(Integer::compareTo).get();
+//        System.out.println(integer);
     }
 
 }
