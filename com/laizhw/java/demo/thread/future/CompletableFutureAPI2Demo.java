70d179480d0e71029d37b7e81d49e105c905f0a5
Laizw <zwlai89@gmail.com>
2022-06-18 14:59:24 +0800

    2022-06-18

--- com/laizhw/java/demo/thread/future/CompletableFutureAPI2Demo.java
+++ com/laizhw/java/demo/thread/future/CompletableFutureAPI2Demo.java
@@ -9,29 +9,31 @@ public class CompletableFutureAPI2Demo {
 
         // thenApply Function 类型接口，一个输入，一个输出， 无法处理异常，中断执行
         // handle BiFunction 类型接口，两个输入，一个输出， 可以处理异常， 继续执行
-        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
+        try {
+            CompletableFuture.supplyAsync(() -> {
+                try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println("--- first step ---");
+                return 1;
+            }, threadPool).handle((f, e) -> {
+                System.out.println("--- second step ---");
+                return f + 2;
+            }).thenApply((f) -> {
+                System.out.println("--- third step ---");
+                return f + 3;
+            }).whenComplete((v, e) -> {
+                if (e == null) {
+                    System.out.println("--- 计算结果为: " + v);
+                }
+            }).exceptionally((e) -> {
                 e.printStackTrace();
-            }
-            System.out.println("--- first step ---");
-            return 1;
-        }, threadPool).handle((f, e) -> {
-            System.out.println("--- second step ---");
-            return f + 2;
-        }).thenApply((f) -> {
-            System.out.println("--- third step ---");
-            return f + 3;
-        }).whenComplete((v, e) -> {
-            if (e == null) {
-                System.out.println("--- 计算结果为: " + v);
-            }
-        }).exceptionally((e) -> {
+                System.out.println(e.getMessage());
+                return null;
+            });
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(e.getMessage());
-            return null;
-        });
+        } finally {
+            threadPool.shutdown();
+        }
 
         System.out.println("--- " + Thread.currentThread().getName() + " ---");
 
