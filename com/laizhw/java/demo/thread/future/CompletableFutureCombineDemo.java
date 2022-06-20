3219a413f55822b6a1982000aa321f39ecc63d36
Laizw <zwlai89@gmail.com>
2022-06-20 11:03:14 +0800

    2022-06-20

---
+++ com/laizhw/java/demo/thread/future/CompletableFutureCombineDemo.java
@@ -0,0 +1,37 @@
+package demo.thread.future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+public class CompletableFutureCombineDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + " 启动");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "play A";
+        });
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName() + " 启动");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "play B";
+        });
+
+        CompletableFuture<String> result = completableFuture1.thenCombine(completableFuture2, (x, y) -> {
+            System.out.println("两个结果开始合并");
+            return x + y;
+        });
+
+        System.out.println(Thread.currentThread().getName() + "\t ---" + result.join());
+
+    }
+}
