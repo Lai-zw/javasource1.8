39b10eac91c17a9a223fe4fe5367f7a4ea64adde
Laizw <zwlai89@gmail.com>
2022-06-20 11:07:26 +0800

    2022-06-20

--- com/laizhw/java/project/model/JudgeRole.java
+++ com/laizhw/java/project/model/JudgeRole.java
@@ -2,7 +2,7 @@ package project.model;
 
 public class JudgeRole {
     public String judge( String roleName ) {
-        // 一行代码搞定！之前的if/else灰飞烟灭
+
         return RoleEnum.valueOf(roleName).op();
     }
 }
--- com/laizhw/java/project/model/MyClassLoader.java
+++
@@ -1,9 +0,0 @@
-package project.model;
-/**
- * TODO
- * @className    : MyClassLoader
- * @author       : LaiZhw
- * @version      : v1.0
- */
-public class MyClassLoader {
-}
